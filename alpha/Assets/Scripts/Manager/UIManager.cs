62a784d350fc7edbf55573184cdb1838cd2a0c91
DevTao <1107689123@qq.com>
2023-06-04 00:34:03 +0800

    新增ui基础

--- alpha/Assets/Scripts/Manager/UIManager.cs
+++ alpha/Assets/Scripts/Manager/UIManager.cs
@@ -2,17 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// uiπ‹¿Ì∆˜
+/// </summary>
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OpenUI(string ui)
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CloseUI(string ui)
     {
-        
     }
 }
