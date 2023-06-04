4c9a2efe1734b7dc3a6c55e2810a81caad0a5b80
DevTao <1107689123@qq.com>
2023-06-04 21:45:02 +0800

    UIManager初始化时加载UIRoot

--- alpha/Assets/Scripts/UI/UIManager.cs
+++ alpha/Assets/Scripts/UI/UIManager.cs
@@ -8,8 +8,12 @@ using UnityEngine;
 [RegisterSystem(Const.EInitPriority.UI)]
 public class UIManager : SystemBase<UIManager>, IUpdatable
 {
+    private GameObject m_UIRoot;
     public override void OnInit()
     {
+        var asset = Resources.Load<GameObject>("UIRoot");
+        m_UIRoot = GameObject.Instantiate(asset);
+        GameObject.DontDestroyOnLoad(m_UIRoot);
     }
 
     public override void OnReset()
