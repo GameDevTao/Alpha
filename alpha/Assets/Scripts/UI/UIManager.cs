0f28c28ae266cd2b027a9194387f7bb10f55deb8
DevTao <1107689123@qq.com>
2023-06-04 11:45:53 +0800

    修改ui

---
+++ alpha/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// uiπ‹¿Ì∆˜
+/// </summary>
+public class UIManager : MonoBehaviour
+{
+    public void OpenUI(string ui)
+    {
+
+    }
+
+    public void CloseUI(string ui)
+    {
+    }
+}
